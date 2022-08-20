1fa3769b5b2788dcb7395ad96f1b045f381b2ec5
Arghyadeep Mukherjee <arghyadeepbumbum@gmail.com>
2022-08-20 19:13:16 +0530

    Initial commit

---
+++ JS/app.js
@@ -0,0 +1,51 @@
+const responsive = {
+  0: {
+    items: 1,
+  },
+  320: {
+    items: 1,
+  },
+  560: {
+    items: 2,
+  },
+  960: {
+    itmes: 3,
+  },
+};
+
+$(document).ready(function () {
+  $navbar = $(".navbar");
+  $toggleCollapse = $(".toggle-collapse");
+
+  // click event on toggle menu
+  $toggleCollapse.click(function () {
+    $navbar.toggleClass("collapse");
+  });
+
+  // owl-carousel for blog
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [
+      $(".owl-navigation .owl-nav-prev"),
+      $(".owl-navigation .owl-nav-next"),
+    ],
+    responsive: responsive,
+  });
+
+  //click to scroll up
+  $(".move-up span").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      1000
+    );
+  });
+
+  // AOS Instance
+  AOS.init();
+});
